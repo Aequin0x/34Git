ad53d536f97ea69109875eded92f9cc04776d875
Aequin0x <thibaut.desrame@gmail.com>
2016-06-30 16:56:39 +0200

    valid files extension
    jpg, jpeg, gif, png => ok

--- upload.php
+++ upload.php
@@ -22,16 +22,20 @@
 <?php 
 if(isset($_FILES['files'])){
 	$limit = 512;
+	// Un tableau avec les extensions valides
+	$checkExts = array("jpg", "jpeg", "gif", "png");
+
 	$name = uniqid().$_FILES['files']['name'];
+	$extension = pathinfo($_FILES['files']['name'])['extension'];
 	// Je converto la taille du fichier en Ko
 	$size = $_FILES['files']['size'] / 1024;
-	if($size < $limit){
+	if($size < $limit && in_array($extension, $checkExts)){
 		if(!is_dir('upload')){
 			mkdir('upload');
 		}
 		move_uploaded_file($_FILES['files']['tmp_name'], __DIR__."/upload/".$name);
 	}else{
-		echo "<h1 style='color:red'>Votre fichier est trop volumineux </h1>";
+		echo "<h1 style='color:red'>Votre fichier est trop volumineux ou le format n'est pas pris en charge</h1>";
 	}
 }
 ?>
