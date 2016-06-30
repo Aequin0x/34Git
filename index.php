075ad45b042712986a414618e3d33b9d656be5ab
Aequin0x <thibaut.desrame@gmail.com>
2016-06-30 11:50:57 +0200

    Use file_get_content
    More faster tech

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
 
-// Pour ouvrir, lire et fermer un fichier
+// Pour ouvrir, lire et fermer un fichier //
 
 $file = fopen("texte.txt", "r");
 var_dump(fread($file, filesize('texte.txt')));
 fclose($file);
  
-// Pour ecrire dans un fichier
+// Pour ecrire dans un fichier //
 
 // r = read
 // w+ = ecrire et remplace le texte
@@ -16,5 +16,9 @@ fclose($file);
 $file = fopen('texte.log', "a");
 fwrite($file, date("Y-m-d H:i:s")." : Le script à été ouvert\r\n");
 fclose($file);
+
+// Fonction equivalentes
+
+echo file_get_contents('texte.log'); // equivalent de fopen>fread>fclose
 
 ?>
