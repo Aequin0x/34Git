a5f9ff19ca81962a6f1c507ed4803c6c3aa172bb
Aequin0x <thibaut.desrame@gmail.com>
2016-06-30 16:14:17 +0200

    add upload possibility
    + css dir

--- upload.php
+++ upload.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Upload de fichier</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<form enctype="multipart/form-data" method="POST">
+		<fieldset>
+			<label>Fichier : </label>
+			<br>
+			<br>
+			<input type="file" name="files">
+			<br>
+			<br>
+			<button>Envoie du fichier</button>
+		</fieldset>
+	</form>
+</body>
+</html>
 <?php 
-
+if(isset($_FILES['files'])){
+	var_dump($_FILES);
+	$name = uniqid().$_FILES['files']['name'];
+	move_uploaded_file($_FILES['files']['tmp_name'], __DIR__."/img/".$name);
+}
 ?>
