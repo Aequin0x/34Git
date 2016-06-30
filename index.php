5fd1de0182e31a6ed8edf45cfb1af37fa21fd21f
Aequin0x <thibaut.desrame@gmail.com>
2016-06-30 13:46:59 +0200

    add copie de fichiers
    copy("test/texte.log", "test/texte.log.backup");

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ fclose($file);
  
 // Fonction equivalentes pour lire un fichier avec fopen
 
-echo file_get_contents('texte.log'); // equivalent de fopen>fread>fclose
+echo file_get_contents('texte.txt'); // equivalent de fopen>fread>fclose
 
 // Pour ecrire dans un fichier //
 
@@ -17,7 +17,7 @@ echo file_get_contents('texte.log'); // equivalent de fopen>fread>fclose
 // a = ecrire à la fin du fichier en ecriture seul
 // a+ = ecrire à la fin du fichier en ecriture seule + lecture du fichier
 
-$file = fopen('texte.log', "a");
+$file = fopen('texte.log', "a+");
 fwrite($file, date("Y-m-d H:i:s")." : Le script à été ouvert\r\n");
 fclose($file);
 
@@ -51,4 +51,18 @@ if(!is_dir('test')){
 
 // Deplacer ou renommer un fichier
 rename('texte.log', 'test/texte.log');
+
+// Recup le nom du dossier courant
+echo __DIR__;
+// ou
+echo dirname(__FILE__);
+// Recup le chemin du fichier courant
+echo __FILE__;
+
+// Recup la date de dernière modif d'un fichier
+echo date("d/m/Y à H:i:s", filemtime("test/texte.log"));
+
+// Copier un fichier
+copy("test/texte.log", "test/texte.log.backup");
+
 ?>
