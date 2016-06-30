259698cc410e2702023bd77bb706e336646e6b6d
Aequin0x <thibaut.desrame@gmail.com>
2016-06-30 11:39:44 +0200

    Ajoute fichier log

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php 
 
+// Pour ouvrir, lire et fermer un fichier
+
 $file = fopen("texte.txt", "r");
 var_dump(fread($file, filesize('texte.txt')));
 fclose($file);
  
+// Pour ecrire dans un fichier
+
+// r = read
+// w+ = ecrire et remplace le texte
+// a = ecrire à la fin du fichier en ecriture seul
+// a+ = ecrire à la fin du fichier en ecriture seule + lecture du fichier
+
+$file = fopen('texte.log', "a");
+fwrite($file, date("Y-m-d H:i:s")." : Le script à été ouvert\r\n");
+fclose($file);
 
 ?>
